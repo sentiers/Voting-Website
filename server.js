fd16d706cae9d1df4f66877403bcabffa528fb25
daeyeoplee <dyl9912@naver.com>
2021-05-13 12:56:10 +0900

    user.js

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var path = require("path");
 var app = express();
+const { userRouter } = require('../src/userRoute') // lee
+const mongoose = require('mongoose') // lee
+
+//const MONGO_URI = '여기 몽고 DB'
 
 function onHttpStart() {
   console.log("Express http server listening on " + HTTP_PORT);
@@ -14,3 +18,5 @@ app.get("/", function (req, res) {
 });
 
 app.listen(HTTP_PORT, onHttpStart);
+
+app.use('/user', userRouter); // 이거 강의에 들어가있는데 뭔지 잘 모르겠...
